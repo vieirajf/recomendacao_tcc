2ad01bb98af7e4cd99704d593c0d0919d1e149be
vieirajf <vieirajf@gmail.com>
2015-01-20 23:04:37 -0300

    Incluido metodo para pegar tweets

--- src/main/java/br/ce/qxa/ufc/web/TwitterController.java
+++ src/main/java/br/ce/qxa/ufc/web/TwitterController.java
@@ -9,10 +9,7 @@ import javax.validation.Valid;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import twitter4j.IDs;
 import twitter4j.Paging;
@@ -20,20 +17,18 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.User;
 import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 import br.ce.qxa.ufc.model.AutorizacaoTwitter;
-import br.ce.qxa.ufc.model.Papel;
 import br.ce.qxa.ufc.model.Pin;
+import br.ce.qxa.ufc.model.TwitterUsuarioId;
 import br.ce.qxa.ufc.model.Usuario;
-import br.ce.qxa.ufc.repositorio.jpa.GenericRepositoryImpl;
 import br.ce.qxa.ufc.service.GenericService;
 import br.ce.qxa.ufc.service.UsuarioService;
 
 @Controller
-@RequestMapping("/usuario")
-public class UsuarioController {
+@RequestMapping("/twitter")
+public class TwitterController {
 
 	@Inject
 	private UsuarioService usuarioService;
@@ -42,57 +37,17 @@ public class UsuarioController {
 	private GenericService<AutorizacaoTwitter> autorizacaoTwitterService;
 	
 	@Inject
-	private GenericRepositoryImpl<Papel> papelService;
+	private GenericService<TwitterUsuarioId> twitterUsuarioIsService;
+		
 	
-	//private Twitter twitter;
 	
 	private RequestToken requestToken=null;
 
-	@RequestMapping(value = "/adicionar")
-	public String adicionar(ModelMap modelMap) {
-		modelMap.addAttribute("usuario", new Usuario());
-		return "usuario/adicionar";
-	}
-
-	@RequestMapping(value = "/listar")
-	public String listar(ModelMap modelMap) {
-		modelMap.addAttribute("usuario", usuarioService.getUsuarioByLogin(SecurityContextHolder.getContext().getAuthentication().getName()));
-		return "usuario/listar";
-	}
 	
-	@RequestMapping(value = "/adicionar", method = RequestMethod.POST)
-	public String adicionar(@Valid Usuario usuario, BindingResult result,
-			RedirectAttributes redirectAttributes) {
-		if (result.hasErrors()) {
-			return "usuario/adicionar";
-		}
-
-		if (usuarioService.getUsuarioByLogin(usuario.getLogin()) != null) {
-			result.rejectValue("login", "Repeat.usuario.login",
-					"Ja existe um usuario com esse login");
-			return "usuario/adicionar";
-		}
-		usuario.setHabilitado(true);
-		Long id = new Long(1);
-		Papel papel = papelService.find(Papel.class, id);
-
-		List<Papel> papeis = new ArrayList<Papel>();
-		papeis.add(papel);
-		usuario.setPapeis(papeis);
-
-		usuarioService.save(usuario);
-		redirectAttributes.addFlashAttribute("info",
-				"Usuario adicionado com sucesso.");
-		return "redirect:/login";
-
-	}
-
 	@RequestMapping(value = "/autorizacaoTwitter")
 	public String autorizacaoTwitter(@Valid Pin pin,ModelMap modelMap) throws Exception {
 		Usuario usuarioAutorizado = usuarioService.getUsuarioByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
 		AccessToken accessToken = null;
-		Twitter twitter = null;
-		
 		try{
 	           if(pin.getPin().length() > 0){
 	        	 accessToken = twitter.getOAuthAccessToken(requestToken, pin.getPin());
@@ -118,88 +73,12 @@ public class UsuarioController {
 		usuarioService.update(usuarioAutorizado);
 		
 		modelMap.addAttribute("usuario",usuarioAutorizado);
-		modelMap.addAttribute("twitter",twitter);
 	return "redirect:/usuario/listar";
 		
 	}
 	
-	public void getIdsUsuariosTwitter(Long idUsuarioTwitter,Twitter twitter) {
-		long cursor = -1;
-        IDs ids = null;
-        User user = null;
-        do {
-            
-          	  //pode usar essa metodo passando como primeiro argumento os id´s e como segundo argumento paginação para retornar os amigos do id repassado.
-                try {
-					ids = twitter.getFriendsIDs(idUsuarioTwitter, cursor);
-				} catch (TwitterException e) {
-					e.printStackTrace();
-				}
-            for (long id : ids.getIDs()) {
-          	  //showuser tem limite de duas vezes por dia
-//          	  try {
-//				user = twitter.showUser(id);
-//			} catch (TwitterException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-          	  
-          	  System.out.println("ID: "+ id);
-//                System.out.println("Nome: "+user.getName());
-                //System.out.println("Quantidades de pessoas: "+user.getFriendsCount());
-                //System.out.println("Status: "+user.getStatus().getText());
-            }
-        } while ((cursor = ids.getNextCursor()) != 0);
-
-	}
 	
-	@RequestMapping(value = "/verificaAcessoTwitter")
-	public String verificaAcessoTwitter(ModelMap modelMap) throws Exception {
-		Usuario usuarioAutorizado = usuarioService.getUsuarioByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
-		Twitter twitter;
-		if (usuarioAutorizado.getAutorizacaoTwitter() != null) {
-			twitter = new TwitterFactory().getInstance();
-			twitter.setOAuthConsumer("bVqAzGbuR5jsOTDstph9XB1dM","vi9xVqIc1oMQAydQYIVgbo0GvO4XWwPjdhtJpjAUk6yv19vdDO");
-			 AccessToken accessToken = new AccessToken(usuarioAutorizado.getAutorizacaoTwitter().getToken(), usuarioAutorizado.getAutorizacaoTwitter().getTokenSecret());  
-		      twitter.setOAuthAccessToken(accessToken);
-			getIdsUsuariosTwitter(usuarioAutorizado.getAutorizacaoTwitter().getId(),twitter);
-
-			Integer i = 1;
-			Integer cont = 1;
-			System.out.println("Showing home timeline.");
-			List<Status> statuses = twitter.getUserTimeline(729614881,new Paging(i, 200));
-			while(!statuses.isEmpty()){
-			 // The factory instance is re-useable and thread safe.
-		    //729614881
-			   	for (Status status : statuses) {
-			        System.out.println(cont +": "+status.getUser().getName() + ":" +  status.getText());
-			        cont++;
-			       
-			    }
-		    		        	
-		    i++;
-		     statuses = twitter.getUserTimeline(729614881,new Paging(i, 200));
-			 }
-			return "redirect:/usuario/listar";
-			
-			
-		}	
-		twitter = new TwitterFactory().getInstance();
-		twitter.setOAuthConsumer("bVqAzGbuR5jsOTDstph9XB1dM","vi9xVqIc1oMQAydQYIVgbo0GvO4XWwPjdhtJpjAUk6yv19vdDO");
-
-		requestToken = twitter.getOAuthRequestToken();
-		//AccessToken accessToken = null;
-		String url = requestToken.getAuthorizationURL();
-		System.out.println(url);
-		System.out.println(url);
-		System.out.println(url);
-		System.out.println(url);
-		modelMap.addAttribute("url",url);
-		modelMap.addAttribute("pin", new Pin());
-		return "usuario/autorizacaoTwitter";	
-	}
-	
-	public void getTweets(Twitter twitter){
+	public void getTwitte(Twitter twitter){
 		Integer i = 1;
 		Integer cont = 1;
 		System.out.println("Showing home timeline.");
@@ -224,6 +103,65 @@ public class UsuarioController {
 		}
 		
 	
+	}
+	public void getIdsUsuariosTwitter(Long idUsuarioTwitter, Usuario usuario) {
+		long cursor = -1;
+        IDs ids = null;
+//        User user;
+        List <TwitterUsuarioId> idsUsuarioTwitter = new ArrayList<TwitterUsuarioId>();
+        do {
+            
+          	  //pode usar essa metodo passando como primeiro argumento os id´s e como segundo argumento paginação para retornar os amigos do id repassado.
+                try {
+					ids = twitter.getFriendsIDs(idUsuarioTwitter, cursor);
+				} catch (TwitterException e) {
+					e.printStackTrace();
+				}
+                
+            for (long id : ids.getIDs()) {
+          	  //showuser tem limite de duas vezes por dia
+          	  //user = twitter.showUser(id);
+            	TwitterUsuarioId novosIds = new TwitterUsuarioId();
+            	novosIds.setIdTwitter(id);
+            	twitterUsuarioIsService.save(novosIds);
+          	  idsUsuarioTwitter.add(novosIds);
+          	//  System.out.println("ID: "+ id);
+                //System.out.println("Nome: "+user.getName());
+                //System.out.println("Quantidades de pessoas: "+user.getFriendsCount());
+                //System.out.println("Status: "+user.getStatus().getText());
+            }
+        } while ((cursor = ids.getNextCursor()) != 0);
+        usuario.setAmigosId(idsUsuarioTwitter);
+        usuarioService.update(usuario);
+
+	}
+	
+	@RequestMapping(value = "/verificaAcessoTwitter")
+	public String verificaAcessoTwitter(ModelMap modelMap) throws Exception {
+		Usuario usuarioAutorizado = usuarioService.getUsuarioByLogin(SecurityContextHolder.getContext().getAuthentication().getName());
+		if (usuarioAutorizado.getAutorizacaoTwitter() != null) {
+			twitter = new TwitterFactory().getInstance();
+			twitter.setOAuthConsumer("bVqAzGbuR5jsOTDstph9XB1dM","vi9xVqIc1oMQAydQYIVgbo0GvO4XWwPjdhtJpjAUk6yv19vdDO");
+			 AccessToken accessToken = new AccessToken(usuarioAutorizado.getAutorizacaoTwitter().getToken(), usuarioAutorizado.getAutorizacaoTwitter().getTokenSecret());  
+		      twitter.setOAuthAccessToken(accessToken);
+			getIdsUsuariosTwitter(usuarioAutorizado.getAutorizacaoTwitter().getId(), usuarioAutorizado);
+			return "redirect:/usuario/listar";
+			
+			
+		}	
+		twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer("bVqAzGbuR5jsOTDstph9XB1dM","vi9xVqIc1oMQAydQYIVgbo0GvO4XWwPjdhtJpjAUk6yv19vdDO");
+
+		requestToken = twitter.getOAuthRequestToken();
+		//AccessToken accessToken = null;
+		String url = requestToken.getAuthorizationURL();
+		System.out.println(url);
+		System.out.println(url);
+		System.out.println(url);
+		System.out.println(url);
+		modelMap.addAttribute("url",url);
+		modelMap.addAttribute("pin", new Pin());
+		return "usuario/autorizacaoTwitter";	
 	}
 
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -1,4 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
-<c:redirect url="/usuario/verificaAcessoTwitter"></c:redirect>
+<c:redirect url="/usuario/verificaAcessoTwitter">
+<c:param name="twitter" value="${twitter}">
+</c:param>
+</c:redirect>
